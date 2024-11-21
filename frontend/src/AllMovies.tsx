114962f72e850d24483632e9c2d57180410191a9
eerikanttilasiili <eerik.anttila@siili.com>
2024-11-21 17:11:06 +0200

    MainTest1039

--- frontend/src/AllMovies.tsx
+++ frontend/src/AllMovies.tsx
@@ -23,7 +23,7 @@ const AllMovies = () => {
             onClick={getMessage}
             sx={{ display: 'block', margin: '0 auto', marginBottom: '2rem' }}
           >
-            Main 1030
+            Main 1039
           </Button>
           <Button
             variant="contained"
